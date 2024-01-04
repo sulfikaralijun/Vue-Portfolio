68816e710d718d21f7c1069cec1786c63c138ccd
sulfikaralijun <sulfikaralijun@gmail.com>
2024-01-04 17:15:24 +0800

    #0005

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,7 +4,13 @@
 
 <template>
   <section id="home">
-    
+    <div class="text-section">
+      <strong>Hi there I'm </strong>
+      <h1>Sulfikar Alijun</h1>
+      <p class="desc">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi ipsum dolorum accusantium. Odit, laborum facere? Nostrum est architecto ducimus nam?
+      </p>
+    </div>
   </section>
 </template>
 
@@ -12,5 +18,8 @@
 section#home {
   width: 100%;
   min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
