caff3e8f296c14d00a44a2b0bf3125d9254f5f3e
sulfikaralijun <sulfikaralijun@gmail.com>
2024-01-04 17:58:43 +0800

    #0013

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -70,6 +70,18 @@ section#home .img-section {
 .img-section img {
   width: 400px;
   border-radius: 30% 70% 70% 30% / 30% 35% 65% 70% ;
+  animation: rotate 5s ease-in-out infinite;
 }
-
+@keyframes rotate {
+  0%,100% {
+    border-radius: 30% 70% 70% 30% / 30% 35% 65% 70% ;
+    
+  } 25%{
+    border-radius: 70% 30% 30% 70% / 70% 70% 30% 30%  ;
+  }50%{
+    border-radius: 70% 30% 70% 30% / 30% 30% 70% 70%  ;
+  }75%{
+    border-radius: 30% 70% 70% 30% / 70% 70% 30% 30%  ;
+  }
+}
 </style>
