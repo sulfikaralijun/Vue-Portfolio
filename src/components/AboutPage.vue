95560c244fc47b3f1fd9d5291bd346fb96be6123
sulfikaralijun <sulfikaralijun@gmail.com>
2024-01-05 19:22:53 +0800

    feet: add template

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,5 +1,38 @@
 <template>
-  <section>
-    
+  <section id="about">
+    <BannerIconVue width="300px"/>
+    <div class="about-text">
+      <h3>About</h3>
+      <p>Hello, I'm Sulfikar Alijun, a beginner front-end developer with skills in HTML, CSS, and JavaScript. I'm passionate about creating captivating and responsive user experiences. With a strong understanding of UI/UX design, I'm ready to collaborate in a team to deliver visually appealing and functional websites.</p>
+    </div>
   </section>
 </template>
+<script setup>
+import BannerIconVue from './child/BannerIcon.vue';
+
+</script>
+<style scoped>
+section#about {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.about-text {
+  width: 50%;
+  /* background-color: blue; */
+}
+.about-text h3 {
+  font-size: 2rem;
+  background: linear-gradient(45deg, var(--primary-color), var(--secondary-color), var(--primary-color)), var(--secondary-color);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+.about-text p {
+  width: 60%;
+  font-size: 1rem;
+  color: var(--black);
+}
+</style>
